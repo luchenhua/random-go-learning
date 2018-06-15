cab0b93a5f18a1fdf38c1177927662b63d3cf82d
Chenhua Lu <luchenhua.840603@gmail.com>
2018-06-15 19:52:14 +0800

    拆分主方法中的路由定义，具体实现单独抽出成为一个方法。

--- example.go
+++ example.go
@@ -1,27 +1,28 @@
 package main
 
 import (
-	"github.com/gin-gonic/gin"
 	"net/http"
+
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
 
 	r := gin.Default()
 
-	r.GET(
-		"/test",
-		func(c *gin.Context) {
-			var result struct {
-				Data    string `json:"data"`
-				Message string `json:"msg"`
-			}
-
-			result.Data = "Go REST service"
-			result.Message = "No message"
-
-			c.JSON(http.StatusOK, result)
-		})
+	r.GET("/test", test)
 
 	r.Run(":8081")
+}
+
+func test(c *gin.Context) {
+	var result struct {
+		Data    string `json:"data"`
+		Message string `json:"msg"`
+	}
+
+	result.Data = "Go REST service"
+	result.Message = "No message"
+
+	c.JSON(http.StatusOK, result)
 }
