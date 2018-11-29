6d20bde6a0d92528ca9b1a7d19ed0cf9ae94a33d
Chenhua Lu <luchenhua.840603@gmail.com>
2018-11-29 21:03:21 +0800

    Add rest with standard lib.

---
+++ rest-with-stdlib.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", hello)
+	http.ListenAndServe(":8081", nil)
+}
+
+func hello(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hello, rest with standard lib."))
+}
